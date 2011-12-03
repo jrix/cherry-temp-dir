edad5777ecc06be6a5d3ed26ed0bb524ddeeab24
cherry980711@gmail.com <cherry980711@gmail.com@a956067f-6103-70aa-fa4a-e20a4e43ff51>
2011-12-03 16:18:40 +0000

    打印关节

--- trunk/cherry-temp-dir/KinectDevice/KinectDevice_i.c
+++ trunk/cherry-temp-dir/KinectDevice/KinectDevice_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Thu Dec 01 21:22:22 2011
+/* at Sat Dec 03 12:32:48 2011
  */
 /* Compiler settings for .\KinectDevice.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
--- trunk/cherry-temp-dir/KinectDevice/NiUserDetector.h
+++ trunk/cherry-temp-dir/KinectDevice/NiUserDetector.h
@@ -1,8 +1,7 @@
 #include <XnCppWrapper.h>
+#include <XnHash.h>
 #include <XnList.h>
-
 XN_DECLARE_DEFAULT_HASH(XnSkeletonJoint,XnSkeletonJointTransformation,XnUserSkeleton);
-
 XN_DECLARE_DEFAULT_HASH(XnUserID,XnUserSkeleton,XnUserSkeletonSet);
 
 class NiUserDetector{	
@@ -13,18 +12,24 @@ public:
 	XnStatus Run();
 	void Stop();
 	XnUserID getPlayer();
+	void getUserAllJoint(XnUserID user,XnUserSkeletonSet uss_hash);//
+	void getAllJoint(XnUserID user,XnUserSkeleton us_hash);
+	XnStatus getAllUser(XnUserID *  pUsers, XnUInt16&  pnUsers );
+
+
+
 private:
 	static void XN_CALLBACK_TYPE UserExit(xn::UserGenerator &generator, XnUserID user, void *pCookie);
 	static void XN_CALLBACK_TYPE UserReEnter(xn::UserGenerator &generator, XnUserID user, void *pCookie);
 	static void XN_CALLBACK_TYPE NewUser(xn::UserGenerator &generator, XnUserID user, void *pCookie);
 	static void XN_CALLBACK_TYPE LostUser(xn::UserGenerator &generator, XnUserID user, void *pCookie);
+	static void XN_CALLBACK_TYPE poseDetected(xn::UserGenerator &generator, XnUserID user, void *pCookie);
 	static void XN_CALLBACK_TYPE UserCalibrationComplete(xn::SkeletonCapability& capability, XnUserID nId, XnCalibrationStatus eStatus, void* pCookie);
 	
 	xn::Context g_context;
 	xn::UserGenerator g_usrGen;
 	XnCallbackHandle hUserExit,hUserReEnter,hUserCallbacks,hCalibration;
 	XnUserID playerId;
-	XnBool cali_flag;
 	static XnList sm_Instances;
 };
 
