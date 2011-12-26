d5e3466787bcbaece24812b611b955175b736125
cherry980711@gmail.com <cherry980711@gmail.com@a956067f-6103-70aa-fa4a-e20a4e43ff51>
2011-12-26 10:26:09 +0000

    ScanProj

---
+++ trunk/cherry-temp-dir/ScanPerson/ScanPerson/ScanPerson_i.c
@@ -0,0 +1,85 @@
+
+
+/* this ALWAYS GENERATED file contains the IIDs and CLSIDs */
+
+/* link this file in with the server and any clients */
+
+
+ /* File created by MIDL compiler version 7.00.0500 */
+/* at Mon Dec 26 17:47:30 2011
+ */
+/* Compiler settings for .\ScanPerson.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run)
+    protocol : dce , ms_ext, c_ext, robust
+    error checks: stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+//@@MIDL_FILE_HEADING(  )
+
+#pragma warning( disable: 4049 )  /* more than 64k source lines */
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#include <rpc.h>
+#include <rpcndr.h>
+
+#ifdef _MIDL_USE_GUIDDEF_
+
+#ifndef INITGUID
+#define INITGUID
+#include <guiddef.h>
+#undef INITGUID
+#else
+#include <guiddef.h>
+#endif
+
+#define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
+        DEFINE_GUID(name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8)
+
+#else // !_MIDL_USE_GUIDDEF_
+
+#ifndef __IID_DEFINED__
+#define __IID_DEFINED__
+
+typedef struct _IID
+{
+    unsigned long x;
+    unsigned short s1;
+    unsigned short s2;
+    unsigned char  c[8];
+} IID;
+
+#endif // __IID_DEFINED__
+
+#ifndef CLSID_DEFINED
+#define CLSID_DEFINED
+typedef IID CLSID;
+#endif // CLSID_DEFINED
+
+#define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
+        const type name = {l,w1,w2,{b1,b2,b3,b4,b5,b6,b7,b8}}
+
+#endif !_MIDL_USE_GUIDDEF_
+
+MIDL_DEFINE_GUID(IID, IID_IScanPerson,0xafb35e79,0x34aa,0x4cb5,0x89,0x84,0x63,0x1d,0xcf,0x3a,0xd6,0x22);
+
+
+MIDL_DEFINE_GUID(IID, LIBID_ScanPersonLib,0x17EFE0D4,0x1279,0x421C,0xBB,0x02,0x21,0xAB,0x9B,0xA0,0xFD,0xBA);
+
+
+MIDL_DEFINE_GUID(CLSID, CLSID_ScanPerson,0x70174573,0xe3f9,0x4f69,0x94,0x67,0x62,0xaf,0xd6,0x54,0x4b,0x31);
+
+#undef MIDL_DEFINE_GUID
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
